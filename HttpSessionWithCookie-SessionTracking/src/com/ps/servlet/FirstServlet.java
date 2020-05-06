6dfaaf4e11cc7cf00a8197572774f3bf71a3da7f
Lenovo <Lenovo@DESKTOP-NHBQ8G6>
2020-05-06 20:22:49 +0530

    SessionTrackingHttpWithCookie

---
+++ HttpSessionWithCookie-SessionTracking/src/com/ps/servlet/FirstServlet.java
@@ -0,0 +1,69 @@
+package com.ps.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/firsturl")
+public class FirstServlet extends HttpServlet {
+	   
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		PrintWriter pw=null;
+		String name=null,fname=null,gender=null;
+		HttpSession ses=null;
+		long ms=0L;
+		Date d=null;
+		boolean b=false;
+		
+		//get PrintWriter
+		pw=res.getWriter();
+		//set Content Type
+		res.setContentType("text/html");
+		//get request parameter value
+		name=req.getParameter("pname");
+		fname=req.getParameter("fname");
+		gender=req.getParameter("gender");
+		
+		//create HttpSession and add form1/req1 data
+		ses=req.getSession(true);
+		ses.setAttribute("pname", name);
+		ses.setAttribute("fname", fname);
+		ses.setAttribute("gender", gender);
+		
+		//HttpSession actions
+		ms=ses.getLastAccessedTime(); //To know when the session obj is lastly accessed
+		d=new Date(ms);
+		
+		b=ses.isNew(); //To know wheather session is new or old
+				
+		//create dynamic form2
+		pw.println("<form action='secondurl' method='POST'>");
+		pw.println("<table align='center' border='0' bgcolor='green'>");
+		pw.println("<tr><td>Income ::</td><td><input type='text' name='income'></td></tr>");
+		pw.println("<tr><td>Tax ::</td><td><input type='text' name='tax'></td></tr>");
+		pw.println("<tr><td><input type='submit' value='Submit'></td><td><input type='reset' value='Cancel'></td></tr>");
+		pw.println("</table></form>");
+		pw.println("<br><br><b>HttpSession id::: </b>"+ses.getId());		
+		pw.println("<br><br><b>Last access time::: </b>"+d);
+		pw.println("<br><br><b>Session is new ?::: </b>"+b);		
+
+
+		
+		//close stream
+		pw.close();
+		
+	}
+
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doGet(req, res);
+	}
+
+}
---
+++ HttpSessionWithCookie-SessionTracking/src/com/ps/servlet/SecondServlet.java
@@ -0,0 +1,135 @@
+package com.ps.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+@WebServlet("/secondurl")
+public class SecondServlet extends HttpServlet {
+	private static final String INSERT_PERSON_QUERY="INSERT INTO COOKIE_PERSON_INFO VALUES(COOKIE_PID.NEXTVAL,?,?,?,?,?)";
+	
+	@Resource(name="DsJndi")
+	private DataSource ds;
+	   
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		PrintWriter pw=null;
+		String name=null,fname=null,gender=null;
+		Float income=0.0f,tax=0.0f;
+		Connection con=null;
+		PreparedStatement ps=null;
+		int count=0;
+		HttpSession ses=null;
+		long ms=0L;
+		Date d=null;
+		boolean b=false;
+		int time=0;
+		
+		//get PrintWriter
+		pw=res.getWriter();
+		//set Content Type
+		res.setContentType("text/html");
+		
+		//read form2/req2 data
+		income=Float.parseFloat(req.getParameter("income"));
+		tax=Float.parseFloat(req.getParameter("tax"));
+		
+		//read session form1/req1 data
+		ses=req.getSession(false);
+		name=(String) ses.getAttribute("pname");
+		fname=(String) ses.getAttribute("fname");
+		gender=(String) ses.getAttribute("gender");
+		
+		//HttpSession actions
+		ms=ses.getLastAccessedTime(); //To know when the session obj is lastly accessed
+		d=new Date(ms);
+				
+		b=ses.isNew(); //To know wheather session is new or old
+		
+		time=ses.getMaxInactiveInterval(); // TO know session's idle timeout period
+				
+		try {
+			//get connection from connection pooling
+			con=ds.getConnection();
+			//create PreparedStatement query
+			ps=con.prepareStatement(INSERT_PERSON_QUERY);
+			//set query param value
+			if(ps!=null) {
+				ps.setString(1, name);
+				ps.setString(2, fname);
+				ps.setString(3, gender);
+				ps.setFloat(4, income);
+				ps.setFloat(5, tax);
+			}
+			
+			//execute sql query statement
+			count=ps.executeUpdate();
+			
+			//process the result
+			if(count==0)
+				pw.println("<h1 style='color:red;text-align:center'>Person registration failed.</h1>");
+			else
+				pw.println("<h1 style='color:green;text-align:center'>Person registration succeded.</h1>");		
+			
+		}
+		catch(SQLException se) {
+			se.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			//close objects
+			try {
+				if(ps!=null)
+					ps.close();
+			}
+			catch(SQLException se) {
+				se.printStackTrace();
+			}
+			try {
+				if(con!=null)
+					con.close();
+			}
+			catch(SQLException se) {
+				se.printStackTrace();
+			}
+		}//finally
+		
+		//display form1/req1 data and form2/req2 data
+		pw.println("form1/req1 data :: "+name+"......"+fname+"......"+gender);
+		pw.println("<br>form2/req2 data :: "+income+"......"+tax);
+		pw.println("<br><br><b>HttpSession id::: </b>"+ses.getId());
+		
+		//set home page
+		pw.println("<br><br><b>Last access time::: </b>"+d);
+		pw.println("<br><br><b>Session is new ?::: </b>"+b);
+		pw.println("<br><br><b>Session idle timeout period::: </b>"+time);
+		pw.println("<br><br><h1><a href='input.html'>Home</a></h1>");
+		
+		//To invalidate the session
+		ses.invalidate();
+		
+		//close stream
+		pw.close();		
+		
+	}
+
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doGet(req, res);
+	}
+
+}
